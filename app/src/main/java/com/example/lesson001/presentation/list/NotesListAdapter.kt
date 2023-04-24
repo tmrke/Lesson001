80e911869b33824fe2f00240a830088b569f9753
tmrke <namat9@yandex.ru>
2023-04-24 18:37:26 +0700

    added SwipeToDeleteCallback.kt

--- app/src/main/java/com/example/lesson001/presentation/list/NotesListAdapter.kt
+++ app/src/main/java/com/example/lesson001/presentation/list/NotesListAdapter.kt
@@ -3,15 +3,16 @@ package com.example.lesson001.presentation.list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lesson001.data.Note
 import com.example.lesson001.databinding.ItemNoteBinding
 
 class NotesListAdapter : ListAdapter<Note, NotesListAdapter.NoteViewHolder>(diffUtil) {
-
     private var onNoteClick: (Note) -> Unit = {}
     private var onNoteLongClick: (Note) -> Unit = {}
+    private var onSwipeToDeleteItem: (Note) -> Unit = {}
 
     fun setCallback(callback: (Note) -> Unit) {
         this.onNoteClick = callback
@@ -19,6 +20,11 @@ class NotesListAdapter : ListAdapter<Note, NotesListAdapter.NoteViewHolder>(diff
 
     fun setCallbackLong(callback: (Note) -> Unit) {
         this.onNoteLongClick = callback
+    }
+
+    fun setCallbackSwipeToDelete(callback: (Note) -> Unit) {
+        this.onSwipeToDeleteItem = callback
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -35,18 +41,22 @@ class NotesListAdapter : ListAdapter<Note, NotesListAdapter.NoteViewHolder>(diff
         holder.bind(getItem(position))
     }
 
+    fun delete(item: Note) {
+        onSwipeToDeleteItem(item)
+    }
+
+
     inner class NoteViewHolder(
         private val binding: ItemNoteBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-
         fun bind(item: Note) {
             with(binding) {
                 root.setOnClickListener { onNoteClick(item) }
+
                 root.setOnLongClickListener {
                     onNoteLongClick(item)
                     true
                 }
-
 
                 textViewText.text = item.text
             }
@@ -58,3 +68,4 @@ val diffUtil = object : DiffUtil.ItemCallback<Note>() {
     override fun areItemsTheSame(oldItem: Note, newItem: Note) = oldItem.id == newItem.id
     override fun areContentsTheSame(oldItem: Note, newItem: Note) = oldItem == newItem
 }
+
--- app/src/main/java/com/example/lesson001/presentation/list/NotesListFragment.kt
+++ app/src/main/java/com/example/lesson001/presentation/list/NotesListFragment.kt
@@ -2,10 +2,10 @@ package com.example.lesson001.presentation.list
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -24,36 +24,38 @@ class NotesListFragment : Fragment(R.layout.fragment_notes_list) {
 
     private val listAdapter = NotesListAdapter()
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController = Navigation.findNavController(view)
 
         viewModel.getNotes()
 
-        with(binding) {
-            recyclerView.apply {
-                layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
-                adapter = listAdapter.apply {
-                    setCallback { note ->
-                        Toast.makeText(requireContext(), note.text, Toast.LENGTH_SHORT).show()
-                    }
-                }.apply {
-                    setCallbackLong { note ->
-                        viewModel.deleteNote(note.id)
-                    }
+        val recyclerView = binding.recyclerView
+
+
+
+
+        recyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+
+            adapter = listAdapter.apply {
+                setCallbackSwipeToDelete { note ->
+                    viewModel.deleteNote(note.id)
                 }
-
-
             }
+        }
 
-            floatingActionButton.setOnClickListener {
-                navController.navigate(R.id.createNoteFragment)
-            }
+        val swipeToDeleteCallback = SwipeToDeleteCallback(binding.recyclerView.adapter as NotesListAdapter)
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+
+        binding.floatingActionButton.setOnClickListener {
+            navController.navigate(R.id.createNoteFragment)
         }
 
         viewModel.notesListLiveData.observe(viewLifecycleOwner) { list ->
             listAdapter.submitList(list)
-        }
-        //viewLifecycleOwner - жизненный цикл View.  гарантирует, что LiveData будет наблюдаться только тогда, когда View фрагмента существует.
+        } //viewLifecycleOwner - жизненный цикл View.  гарантирует, что LiveData будет наблюдаться только тогда, когда View фрагмента существует.
     }
 }
